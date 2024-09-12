d42ee8c71e56237dfb092cf9dd1115dc396df6c5
kalyanikarde09 <kalyanikarde09@gmail.com>
2024-09-12 21:12:35 +0530

    06 labs

---
+++ src/sept/ex_06092024/Lab007.java
@@ -0,0 +1,16 @@
+package sept.ex_06092024;
+
+public class Lab007 {
+    public static void main(String[] args) {
+//        this code will not be excuted we can write any instruction in it
+//        you can write any thing hear
+        /*
+         *
+         *
+         *Hear i can write any thing
+         * drag the stare
+         *
+         */
+        System.out.println("hi i am kalyani");
+    }
+}
---
+++ src/sept/ex_06092024/Lab008.java
@@ -0,0 +1,17 @@
+package sept.ex_06092024;
+/*Authore -kalyani
+ *
+ * write anything hear
+ *
+ *
+ *
+ */
+
+public class Lab008 {
+//    this is the main function jvm start from hear
+    public static void main(String[] args) {
+//        below code will excute it will print hello world
+        System.out.println("Hello world");
+
+    }
+}
